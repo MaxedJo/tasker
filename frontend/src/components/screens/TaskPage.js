886e3e54a960b0b9e271a42ec263c746968c74ac
MaxedJo <vvmax01@mail.ru>
2023-05-19 16:13:04 +0300

    feat[front] Редактор описания задачи

--- frontend/src/components/screens/TaskPage.js
+++ frontend/src/components/screens/TaskPage.js
@@ -8,6 +8,8 @@ import {fixStatus, validateUser} from "../../utility";
 import Button from "@mui/material/Button";
 import TaskForm from "../ui/TaskForm";
 import Chat from "../ui/Chat";
+import parse from 'html-react-parser';
+import './assets/TaskPage.css';
 
 export default function TaskPage() {
     const load = useLoaderData();
@@ -15,6 +17,7 @@ export default function TaskPage() {
     const [user, setUser] = useState({});
     const [owner, setOwner] = useState({});
     const [edit, setEdit] = useState(true);
+
     useEffect(() => {
         axios.get("http://localhost:8080/user-api/user/"
             + task.owner, {headers: authToken()})
@@ -51,7 +54,8 @@ export default function TaskPage() {
                     <Box>
                         <Typography m={3} variant="h4">{task.title}</Typography>
                         <Typography sx={{whiteSpace: 'pre-line'}} ml="auto" mr="auto" maxWidth={800}
-                                    mb={2}>Описание: {task.description}</Typography>
+                                    className="tmx-description"
+                                    mb={2}>Описание: {parse(task.description)}</Typography>
                         <Button sx={{color: "black"}}
                                 href={"/profile/" + owner.id}>Создатель: {owner ? owner.fio : null}</Button>
                         <Typography/>
