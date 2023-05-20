29e83754880fc684de23b9e36ff83af4f61b3cf1
Maxim <vvmax01@mail.ru>
2023-05-20 10:13:00 +0300

    111

--- src/main/java/ru/vorobev/tasker/controller/TaskFileController.java
+++ src/main/java/ru/vorobev/tasker/controller/TaskFileController.java
@@ -1,9 +1,12 @@
 package ru.vorobev.tasker.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -11,6 +14,8 @@ import ru.vorobev.tasker.controller.dto.FileResponse;
 import ru.vorobev.tasker.model.TaskFile;
 import ru.vorobev.tasker.service.TaskFileService;
 
+import java.io.*;
+import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -58,9 +63,33 @@ public class TaskFileController {
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_TYPE, file.getFileType())
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFileName() + "\"")
-                .header("Filename", file.getFileName())
                 .body(Files.readAllBytes(path));
     }
+
+    @RequestMapping("/file/{fileName:.+}")
+    public void downloadPDFResource(HttpServletRequest request, HttpServletResponse response,
+                                    @PathVariable("fileName") Long id) throws IOException {
+        TaskFile files = taskFileService.findById(id);
+        Path path = Paths.get("files/" + files.getId() + "/" + files.getFileName());
+        File file = path.toFile();
+        if (file.exists()) {
+
+            String mimeType = URLConnection.guessContentTypeFromName(file.getName());
+            if (mimeType == null) {
+                mimeType = "application/octet-stream";
+            }
+
+            response.setContentType(mimeType);
+            response.setHeader("Content-Disposition", String.format("inline; filename=\"" + file.getName() + "\""));
+            response.setContentLength((int) file.length());
+
+            InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+
+            FileCopyUtils.copy(inputStream, response.getOutputStream());
+
+        }
+    }
+
 
     @GetMapping("/delete/{id}")
     public void findById(@PathVariable Long id, Principal principal) {
