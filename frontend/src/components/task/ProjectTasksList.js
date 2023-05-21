9feba67606b061cb2eaac71664251b97674fcff8
MaxedJo <vvmax01@mail.ru>
2023-05-21 17:20:55 +0300

    Фильтрация закрытых задач

--- frontend/src/components/task/ProjectTasksList.js
+++ frontend/src/components/task/ProjectTasksList.js
@@ -1,18 +1,26 @@
 import * as React from 'react';
+import {useEffect} from 'react';
 import TasksList from "./TasksList";
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
 import TaskCreate from "./TaskCreate";
 import {addTask} from "../../api/client";
 import Toolbar from "@mui/material/Toolbar";
-
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
 
 export default function ProjectTasksList(props) {
     const [open, setOpen] = React.useState(false);
     const [tasks, setTasks] = React.useState(props.items);
+    const [filteredTask, setFilteredTask] = React.useState([]);
+    const [showClosed, setShowClosed] = React.useState(false);
     const handleClickOpen = () => {
         setOpen(true);
     };
+
+    useEffect(() => {
+        setFilteredTask(tasks.filter(item => showClosed || (!showClosed && item.status !== 'CLOSED')));
+    }, [showClosed, tasks])
 
     const handleClose = () => {
         setOpen(false);
@@ -27,17 +35,26 @@ export default function ProjectTasksList(props) {
         });
         setOpen(false);
     }
-
+    const toggleChecked = () => {
+        setShowClosed((prev) => !prev);
+    };
     return (
         <>
-            <Toolbar>
+            <Toolbar sx={{
+                display: 'flex',
+                gap: 4
+            }}>
                 <Button
                     startIcon={<AddIcon/>}
                     onClick={handleClickOpen}>
                     Поставить задачу
                 </Button>
+                <FormControlLabel
+                    control={<Switch size="small" checked={showClosed} onChange={toggleChecked}/>}
+                    label="Закрытые"
+                />
             </Toolbar>
-            <TasksList items={tasks}/>
+            <TasksList items={filteredTask}/>
             <TaskCreate
                 open={open}
                 onClose={handleClose}
